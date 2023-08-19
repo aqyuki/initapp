a9dcbd78613079542f5b5f312c66efb15812630d
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-20 07:01:20 +0900

    feat : add function to load file

---
+++ files/file.go
@@ -0,0 +1,20 @@
+package files
+
+import (
+	"io"
+	"os"
+)
+
+func LoadFile(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
