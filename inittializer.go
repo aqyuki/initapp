f0c99ad5cc3bfc2e58fc239621af0ef62f286100
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-20 09:51:16 +0900

    feat : add function to initialize and if dosen't exist file, create new

--- inittializer.go
+++ inittializer.go
@@ -1,25 +1,63 @@
 package initapp
 
 import (
+	"errors"
+	"os"
+
 	"github.com/aqyuki/initapp/files"
 	"github.com/aqyuki/initapp/model"
 )
+
+func decode[T interface{}](ft model.FileType, b []byte) (*T, error) {
+	var data *T
+	var err error
+	switch ft {
+	case model.FileTypeJSON:
+		data, err = decodeJSON[T](b)
+	case model.FileTypeYAML:
+		data, err = decodeYAML[T](b)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return data, err
+}
+
+func encode[T interface{}](ft model.FileType, object *T) ([]byte, error) {
+	var data []byte
+	var err error
+	switch ft {
+	case model.FileTypeJSON:
+		data, err = encodeJSON[T](object)
+	case model.FileTypeYAML:
+		data, err = encodeYAML[T](object)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
 
 func Initialize[T interface{}](cv *model.Converter) (*T, error) {
 	raw, err := files.LoadFile(cv.Config.Path)
 	if err != nil {
 		return nil, err
 	}
+	return decode[T](cv.Config.Type, raw)
+}
 
-	var data *T
-	switch cv.Config.Type {
-	case model.FileTypeJSON:
-		data, err = decodeJSON[T](raw)
-	case model.FileTypeYAML:
-		data, err = decodeYAML[T](raw)
+func InitializeWithDefault[T interface{}](cv *model.Converter, normal T) (*T, error) {
+	raw, err := files.LoadFile(cv.Config.Path)
+	if err == nil {
+		return decode[T](cv.Config.Type, raw)
 	}
-	if err != nil {
-		return nil, err
+	if errors.Is(err, os.ErrNotExist) {
+		b, err := encode[T](cv.Config.Type, &normal)
+		if err != nil {
+			return nil, err
+		}
+		files.WriteFile(cv.Config.Path, b)
+		return &normal, nil
 	}
-	return data, nil
+	return nil, err
 }
